1bec614dcba7061045590bfa634f17f0fd9e7224
dev-abuhurairah <contactabuhurairah@gmail.com>
2026-09-05 10:57:10 +0500

    fix: add missing clickable import in BrowserScreen.kt

--- app/src/main/java/com/snaptube/downloader/ui/screens/BrowserScreen.kt
+++ app/src/main/java/com/snaptube/downloader/ui/screens/BrowserScreen.kt
@@ -6,6 +6,7 @@ import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
