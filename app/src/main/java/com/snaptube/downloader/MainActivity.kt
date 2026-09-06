578677ec8ed7173e55e94618d4b01cafd669ad0d
dev-abuhurairah <contactabuhurairah@gmail.com>
2026-09-06 08:19:32 +0500

    Fixed Crash

--- app/src/main/java/com/snaptube/downloader/MainActivity.kt
+++ app/src/main/java/com/snaptube/downloader/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -282,7 +283,7 @@ private fun ModernNavBarItem(
                     Box(
                         modifier = Modifier
                             .align(Alignment.TopEnd)
-                            .padding(top = (-4).dp, end = (-6).dp)
+                            .offset(x = 6.dp, y = (-4).dp)
                             .size(16.dp)
                             .clip(CircleShape)
                             .background(if (isDownloadingBadge) SnaptubeYellow else Color(0xFF33333E)),
